7c7bfa14f169d29d0cf5f14e7c2aa024f76e56c7
Martin Blech <martinblech@gmail.com>
2012-10-23 17:51:14 -0300

    using vcs_info for prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -79,12 +79,23 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%~%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+export PROMPT=$'\n%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in $(directory_name)\n› '
+
+autoload -Uz vcs_info
+
+zstyle ':vcs_info:*' stagedstr '%F{28}●'
+zstyle ':vcs_info:*' unstagedstr '%F{11}●'
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
+zstyle ':vcs_info:*' enable git svn
+precmd () {
+    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
+        zstyle ':vcs_info:*' formats ' [%F{green}%b%c%u%F{blue}]'
+    } else {
+        zstyle ':vcs_info:*' formats ' [%F{green}%b%c%u%F{red}●%F{blue}]'
+    }
+ 
+    vcs_info
 }
 
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+export RPROMPT=$'${vcs_info_msg_0_}%{$reset_color%}'
