b4c42ca4d964f6a41bb9dce99373c34bb3276bfa
Martin Blech <martinblech@gmail.com>
2014-07-03 16:08:45 -0300

    Updated path for user-installed gems.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,8 @@
-export PATH="/usr/local/share/npm/bin:/usr/local/opt/coreutils/libexec/gnubin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/Applications/Postgres93.app/Contents/MacOS/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/share/npm/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export GEM_HOME=$HOME/.gem/ruby/2.0.0
+export PATH=$GEM_HOME/bin:$PATH
+
+export DOCKER_HOST=tcp://
